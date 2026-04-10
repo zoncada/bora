4d67d0f2fcd586747a4e1fc26d0a6d05b28d9d0a
Bora App <bora@app.com>
2026-04-10 14:39:08 -0400

    fix: corrigir rota wildcard para Express 5 (/{*splat})

--- backend/server.js
+++ backend/server.js
@@ -341,7 +341,7 @@ function getPollWithStats(pollId, userId) {
 }
 
 // Serve frontend
-app.get('*', (req, res) => {
+app.get('/{*splat}', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
